e908f73c20806be03958533095eb3c59085bb45f
Ankesh Kapil <bsi_ankesh@os4w.de>
2022-09-24 10:24:56 +0530

    AssigningVariableToFun

---
+++ src/LambdaExpressions.java
@@ -0,0 +1,10 @@
+package src;
+
+import java.util.function.Function;
+
+public class LambdaExpressions {
+    public static void main(String[] args) {
+        Function<Integer,Integer> lambdaFun = (x)->x+1;
+        System.out.printf(String.valueOf(lambdaFun.apply(5)));
+    }
+}

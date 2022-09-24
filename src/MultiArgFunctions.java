8d4ba804e7340d56a5443772c5fe1248c4473d0c
Ankesh Kapil <bsi_ankesh@os4w.de>
2022-09-24 10:33:35 +0530

    MultiArgFunctions

--- src/MultiArgFunctions.java
+++ src/MultiArgFunctions.java
@@ -4,9 +4,27 @@ import java.util.function.BiFunction;
 
 public class MultiArgFunctions {
     public static void main(String[] args) {
-        //two args
+        //two args comes by default
         BiFunction<Integer,Integer,String> addTwoNums = (a,b)-> String.valueOf(a+b);
 
+        TriFunction<Integer,Integer,Integer,Integer> addThree = (x,y,z)->x+y+z;
+
+        NoArgFunction<Integer> noArgFunction = ()-> 1;
+
         System.out.printf(addTwoNums.apply(99,-99));
+
+        System.out.println(addThree.apply(1,2,3));
+        System.out.println(noArgFunction.apply());
+
     }
+}
+@FunctionalInterface
+interface TriFunction<T,U,V,R>{
+    R apply(T t,U u,V v);
+}
+
+@FunctionalInterface
+interface NoArgFunction<R> {
+
+    R apply();
 }

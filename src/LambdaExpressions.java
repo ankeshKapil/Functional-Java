77dbd680b9cd690241e26586523c0655e9767546
Ankesh Kapil <bsi_ankesh@os4w.de>
2022-09-24 10:27:27 +0530

    LambdaExpressions

--- src/LambdaExpressions.java
+++ src/LambdaExpressions.java
@@ -5,6 +5,10 @@ import java.util.function.Function;
 public class LambdaExpressions {
     public static void main(String[] args) {
         Function<Integer,Integer> lambdaFun = (x)->x+1;
+        Function<String,Integer> multiLine = (a)->{
+            Integer b = a.length();
+            return b;
+        };
         System.out.printf(String.valueOf(lambdaFun.apply(5)));
     }
 }
